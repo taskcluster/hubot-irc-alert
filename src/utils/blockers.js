e3fd7f982985a0b639c004d782ac08ab822cf85e
Dylan William Hardison <dylan@hardison.net>
2019-01-31 15:25:06 -0500

    add more specific useragent and make bugzilla requests faster
    This adds a user-agent that identifies this as tc-hubot, and also makes use of
    the include_fields option. Using this option will make the bugzilla requests
    much faster.

--- src/utils/blockers.js
+++ src/utils/blockers.js
@@ -1,7 +1,12 @@
 const got = require('got');
+const client = got.extend({
+  headers: {
+    'user-agent': 'hubot-irc-alert (https://github.com/taskcluster/hubot-irc-alert)'
+  }
+});
 
 module.exports = async (query) => {
-  const { body: { bugs } } = await got.get(`${process.env.BUGZILLA_BASE_URL}/rest/bug`, {
+  const { body: { bugs } } = await client.get(`${process.env.BUGZILLA_BASE_URL}/rest/bug`, {
     json: true,
     query,
   });
